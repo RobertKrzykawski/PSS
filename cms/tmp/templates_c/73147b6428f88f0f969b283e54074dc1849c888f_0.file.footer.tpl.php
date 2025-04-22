c2c4d119465843b68c19beb885f544a59d194ae8
Robert <104578513+RobertKrzykawski@users.noreply.github.com>
2025-04-22 13:06:46 +0200

    Projekt CMS

---
+++ cms/tmp/templates_c/73147b6428f88f0f969b283e54074dc1849c888f_0.file.footer.tpl.php
@@ -0,0 +1,37 @@
+<?php
+/* Smarty version 4.5.2, created on 2025-04-22 13:02:00
+  from 'C:\xampp\htdocs\cms\admin\themes\OneEleven\templates\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.2',
+  'unifunc' => 'content_6807772833e603_84888476',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '73147b6428f88f0f969b283e54074dc1849c888f' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\cms\\admin\\themes\\OneEleven\\templates\\footer.tpl',
+      1 => 1745319081,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6807772833e603_84888476 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'C:\\xampp\\htdocs\\cms\\lib\\plugins\\function.cms_version.php','function'=>'smarty_function_cms_version',),1=>array('file'=>'C:\\xampp\\htdocs\\cms\\lib\\plugins\\function.cms_versionname.php','function'=>'smarty_function_cms_versionname',),));
+?>
+<footer id="oe_footer" class="cf"><div class="footer-left"><small class="copyright">Copyright &copy; <a rel="external" href="http://www.cmsmadesimple.org">CMS Made Simple&trade; <?php echo smarty_function_cms_version(array(),$_smarty_tpl);?>
+ &ldquo;<?php echo smarty_function_cms_versionname(array(),$_smarty_tpl);?>
+&rdquo;</a></small></div><div class="footer-right cf"><ul class="links"><li><a href="https://docs.cmsmadesimple.org/" rel="external" title="<?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'lang' ][ 0 ], array( 'documentation' ));?>
+"><?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'lang' ][ 0 ], array( 'documentation' ));?>
+</a></li><li><a href="https://forum.cmsmadesimple.org/" rel="external" title="<?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'lang' ][ 0 ], array( 'forums' ));?>
+"><?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'lang' ][ 0 ], array( 'forums' ));?>
+</a></li><li><a href="http://www.cmsmadesimple.org/about-link/" rel="external" title="<?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'lang' ][ 0 ], array( 'about' ));?>
+"><?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'lang' ][ 0 ], array( 'about' ));?>
+</a></li><li><a href="http://www.cmsmadesimple.org/about-link/about-us/" rel="external" title="<?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'lang' ][ 0 ], array( 'team' ));?>
+"><?php echo call_user_func_array($_smarty_tpl->registered_plugins[ 'modifier' ][ 'lang' ][ 0 ], array( 'team' ));?>
+</a></li></ul></div></footer><?php }
+}
